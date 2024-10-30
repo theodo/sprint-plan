21fa9fe429610f45286093a1af5a7f164e38c0ef
moad_fethallah <moad.fethallah@gmail.com>
2024-10-30 17:18:31 +0100

    add auto layout

--- components/blocks/DependencyGraph.tsx
+++ components/blocks/DependencyGraph.tsx
@@ -1,20 +1,75 @@
 "use client";
-
 import {
   addEdge,
   Controls,
+  Edge,
+  Node,
   OnConnect,
+  Panel,
   ReactFlow,
+  ReactFlowProvider,
   useEdgesState,
   useNodesState,
+  useReactFlow,
 } from "@xyflow/react";
-import "@xyflow/react/dist/style.css";
 import { useCallback } from "react";
+import "@xyflow/react/dist/style.css";
+
+import { Button } from "@components/ui/button";
+
+import { getLayoutedElements } from "./utils";
 
 export type Ticket = {
   id: string;
   title: string;
   blockedBy: Ticket[];
+};
+
+const LayoutFlow: React.FC<{
+  initialNodes: Node[];
+  initialEdges: Edge[];
+}> = ({ initialNodes, initialEdges }) => {
+  const { fitView } = useReactFlow();
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onLayout = useCallback(
+    (direction: "TB" | "LR") => {
+      const layouted = getLayoutedElements(nodes, edges, { direction });
+
+      setNodes([...layouted.nodes]);
+      setEdges([...layouted.edges]);
+
+      window.requestAnimationFrame(async () => {
+        await fitView();
+      });
+    },
+    [nodes, edges, setNodes, setEdges, fitView],
+  );
+
+  const onConnect = useCallback<OnConnect>(
+    (params) => {
+      setEdges((_edges) => addEdge(params, _edges));
+    },
+    [setEdges],
+  );
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      fitView
+    >
+      <Panel position="top-right" className="flex gap-2">
+        <Button onClick={() => onLayout("TB")}>vertical layout</Button>
+        <Button onClick={() => onLayout("LR")}>horizontal layout</Button>
+      </Panel>
+      <Controls />
+    </ReactFlow>
+  );
 };
 
 export const DependencyGraph: React.FC<{
@@ -34,27 +89,9 @@ export const DependencyGraph: React.FC<{
     }));
   });
 
-  const [nodes, , onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-  const onConnect = useCallback<OnConnect>(
-    (params) => {
-      setEdges((_edges) => addEdge(params, _edges));
-    },
-    [setEdges],
-  );
-
   return (
-    <>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        fitView
-      >
-        <Controls />
-      </ReactFlow>
-    </>
+    <ReactFlowProvider>
+      <LayoutFlow initialNodes={initialNodes} initialEdges={initialEdges} />
+    </ReactFlowProvider>
   );
 };
